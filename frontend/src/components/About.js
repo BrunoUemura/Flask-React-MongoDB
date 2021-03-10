cf4cfa50cf9fe6fd28855c195a7dba23c024e680
BrunoUemura <bruno.h.uemura@outlook.com>
2021-03-09 23:10:40 -0300

    call backend api and frontend dev

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,3 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-export const About = () => <h1>Hello from about</h1>;
+export const About = () => (
+  <Fragment>
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam convallis est
+      ut eros venenatis fermentum ut quis urna. Nam elementum luctus velit, ac
+      dapibus leo. Sed lorem metus, malesuada et urna condimentum, dignissim
+      tincidunt massa. Cras fringilla ultrices ultricies. Cras malesuada, nisi
+      quis efficitur porta, lectus tellus fermentum nisi, eu tincidunt libero
+      purus vel odio. Maecenas eu nulla et dui ornare ultricies. Morbi porta
+      porta dapibus. Phasellus libero sem, maximus sed pulvinar ut, vehicula vel
+      ligula. In auctor, lacus eu volutpat sodales, tellus felis fringilla nunc,
+      nec fermentum turpis ipsum in arcu. Nunc id lacus et velit pellentesque
+      aliquet nec nec lacus. Sed a nisi at elit molestie malesuada ac quis nunc.
+      Aliquam pharetra nisl id nisl condimentum pulvinar. Morbi commodo iaculis
+      tristique. In aliquam tellus sapien, pharetra scelerisque mauris molestie
+      ut. Nam porttitor tellus et ex consectetur, id aliquam nunc varius.
+      Suspendisse lobortis ullamcorper purus id consectetur.
+    </p>
+    <p>
+      Curabitur vel leo ut enim imperdiet pharetra quis quis arcu. Nam accumsan
+      at sem sollicitudin commodo. Integer at lobortis sapien. Praesent eget
+      accumsan velit, id porttitor sem. Integer lectus nibh, malesuada vel
+      dignissim eget, sodales vel urna. Orci varius natoque penatibus et magnis
+      dis parturient montes, nascetur ridiculus mus. Vestibulum a iaculis erat.
+      Suspendisse potenti.
+    </p>
+  </Fragment>
+);
